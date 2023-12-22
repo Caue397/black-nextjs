b8b47f85e595b134140c7517f2e9726a8dcce8b1
Cauê Gallizzi <cauegallizzi397@gmail.com>
2023-12-21 23:04:27 -0300

    Aplicação finalizada

---
+++ src/components/ProductsDetails.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import { Button, Col, Row } from "reactstrap";
+import { ProductType } from "../pages/services/products";
+import SuccessToast from "./SuccessToast";
+import { useCart } from "@/hooks/useCart";
+
+type ProductDetailsProps = {
+  product: ProductType
+}
+
+const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
+  const [toastIsOpen, setToastIsOpen] = useState(false)
+  const { addProduct } = useCart()
+
+  return (
+    <Row>
+      <Col lg={6} className="d-flex justify-content-center">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={500}
+          height={400}
+          layout="responsive"
+        />
+      </Col>
+
+      <Col lg={6}>
+        <h1>{product.name}</h1>
+
+        <h2 className="text-muted">R$ {product.price}</h2>
+
+        <p className="my-3">
+          <span className="d-block font-weight-bold">Descrição:</span>
+          {product.description}
+        </p>
+
+        <p className="text-muted">Em estoque: {product.inStock}</p>
+
+        <Button
+          color="dark"
+          className="my-3 pb-2"
+          onClick={() => {
+            addProduct(product)
+            setToastIsOpen(true)
+            setTimeout(() => setToastIsOpen(false), 1000 * 3)
+        }}
+        >
+          Compre agora
+        </Button>
+
+        <SuccessToast toastIsOpen={toastIsOpen} setToastIsOpen={setToastIsOpen} />
+      </Col>
+    </Row>
+  )
+}
+
+export default ProductDetails
